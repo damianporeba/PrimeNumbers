f1f132fc599c06fd63270d8451035013c94e48c7
Damian Poręba <damianporeba98@gmail.com>
2023-04-17 18:39:17 +0200

    Add project files.

---
+++ Prime Numbers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+public class PrimeNumber
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Witaj w programie szukającym\nliczb pierwszych w danym przedziale");
+        Console.WriteLine("Podaj wartość początkową przedziału...");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Podak wartość kończącą przedział...");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        
+
+        for(int i = num1; i < num2; i++)
+        {
+
+            int ctr = 0;
+            for(int j = 2; j < i; j++)
+            {
+                if(i!=j && i%j == 0)
+                {
+                    ctr++;
+                    break;
+                }
+            }
+
+            if(ctr==0 && i!=1)
+            {
+                Console.WriteLine(i);
+
+            }
+
+          
+           
+        }
+
+
+
+    }
+
+}
+
